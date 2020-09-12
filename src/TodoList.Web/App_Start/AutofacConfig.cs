f92dd028deb9eacbd42d01b9a1301d8b3e41e60b
簡翊亘(justin.chien)-台灣NEC <justin.chien@nec.com.tw>
2020-09-12 23:17:15 +0800

    fix localdb connection error and test data access layer

---
+++ src/TodoList.Web/App_Start/AutofacConfig.cs
@@ -0,0 +1,56 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TodoList.Entity.DbContextFactory;
+using TodoList.Entity.Interface;
+using TodoList.Entity.Repository;
+using TodoList.Entity.UnitOfWork;
+
+namespace TodoList.Web
+{
+    public class AutofacConfig
+    {
+        public static void Initialize()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            //dbcontext
+            string ConnectionString = ConfigurationManager.ConnectionStrings["TodoListDatabaseEntities"].ConnectionString;
+            var SrcDirectory = System.IO.Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent;
+            var Path = string.Format("{0}\\{1}", SrcDirectory.FullName, "TodoList.Entity");
+            AppDomain.CurrentDomain.SetData("DataDirectory", Path);
+
+            builder.RegisterType<DbContextFactory>()
+                .WithParameter("connectionString", ConnectionString)
+                .As<IDbContextFactory>()
+                .InstancePerLifetimeScope();
+
+            //repository
+            builder.RegisterGeneric(typeof(GenericRepository<>))
+                   .As(typeof(IGenericRepository<>))
+                    .InstancePerLifetimeScope();
+
+            //unit of work
+            builder.RegisterType<EFUnitOfWork>()
+                    .As<IUnitOfWork>()
+                    .InstancePerLifetimeScope();
+
+            ////services
+            //var services = Assembly.Load("NEC.PA.Bussiness");
+            //builder.RegisterAssemblyTypes(services)
+            //    .Where(t => t.Name.EndsWith("Service"))
+            //    .AsImplementedInterfaces();
+
+
+
+            IContainer container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
--- src/TodoList.Web/Controllers/HomeController.cs
+++ src/TodoList.Web/Controllers/HomeController.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TodoList.Entity.Data;
+using TodoList.Entity.Interface;
 
 namespace TodoList.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private IGenericRepository<TodoListItem> _todoListItemRepo;
+        private IUnitOfWork _unitOfWork;
+        public HomeController(IUnitOfWork UnitOfWork)
+        {
+            _unitOfWork = UnitOfWork;
+            _todoListItemRepo = UnitOfWork.Repository<TodoListItem>();
+        }
+
         public ActionResult Index()
         {
+            _todoListItemRepo.Create(new TodoListItem
+            {
+                Id = Guid.NewGuid(),
+                Name = "test",
+                Note = "testsetst",
+                Order = 1
+            });
+            _unitOfWork.SaveChanges();
+            var result = _todoListItemRepo.GetAll().ToList();
             return View();
         }
 
